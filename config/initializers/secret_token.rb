2005b3cfa3b301c9fde78463773a75b45b50430e
Ralphos <wintle.ralph@gmail.com>
2012-08-15 14:16:04 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Friendskis::Application.config.secret_token = 'a05524df44106b998305b94a18e1ce731dcf44a02618c758c19f87a7a4d5108bf3236ba218b0db7846ddd8fa74e4aa38c6aacfb6a4d8b600fdedf6e466b1388a'
