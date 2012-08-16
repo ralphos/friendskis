2b876f1a900d3c89a6967be518ca65f0e8583383
Ralphos <wintle.ralph@gmail.com>
2012-08-16 16:11:55 +0800

    Filled our the photos controller and views

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,10 +1,22 @@
 class PhotosController < ApplicationController
   
   def new
-    @profile_photos = current_user.get_profile_photos
+    @photos = current_user.get_photos
   end
   
   def confirm
+    @photo = Photo.new
     @photo_url = params[:photo_url]
+  end
+  
+  def create
+    @photo = Photo.new(params[:photo])
+    @photo.user_id = current_user.id
+    if @photo.save
+      redirect_to root_url
+    else
+      flash.now.alert = "Sorry there was an error saving your post"
+      render :back
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120815071700) do
+ActiveRecord::Schema.define(:version => 20120816074441) do
+
+  create_table "photos", :force => true do |t|
+    t.string   "thumbnail_url"
+    t.string   "medium_url"
+    t.string   "large_url"
+    t.string   "caption"
+    t.integer  "user_id"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "provider"
