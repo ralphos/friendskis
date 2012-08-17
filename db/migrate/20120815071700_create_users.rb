5c5220cf198cf5b151f4909390d2e5594b7b7ac9
Ralphos <wintle.ralph@gmail.com>
2012-08-17 18:28:41 +0800

    Added a few new columns on the user table

--- db/migrate/20120815071700_create_users.rb
+++ db/migrate/20120815071700_create_users.rb
@@ -8,6 +8,13 @@ class CreateUsers < ActiveRecord::Migration
       t.string :gender
       t.string :oauth_token
       t.datetime :oauth_expires_at
+      t.string :username
+      t.date :date_of_birth
+      t.text :bio
+      t.string :preference
+      t.integer :min_age
+      t.integer :max_age
+      t.string :location
 
       t.timestamps
     end
