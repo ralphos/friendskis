1188a4d0e7d796da6891d4f4b8d6d69ccce391a5
Ralphos <wintle.ralph@gmail.com>
2012-08-17 18:28:24 +0800

    Cleaning up some code

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,21 +21,12 @@ class User < ActiveRecord::Base
   end
   
   def album_covers
-    # just get albums here and then below use the album to get photos via connections
     albums = facebook.get_connections(uid, "albums")
-    covers = albums.map { |h| { id: h["id"], cover_photo: h["cover_photo"] } }
-    covers.map { |c| { id: c[:id], cover_photo: facebook.get_object(c[:cover_photo])["images"][5]["source"] } }
+    albums.map { |h| { id: h["id"], cover_photo: facebook.get_object(h["cover_photo"])["images"][5]["source"] } }
   end
   
   def album_photos(album_id)
     facebook.get_connections(album_id, "photos")
   end
   
-  def get_photos
-    album_hash = facebook.get_connections(uid, "albums")
-    album_ids = album_hash.map { |h| h["id"] }
-    album_photos = facebook.get_connections(album_ids[1], "photos")
-    photo_links = album_photos.map { |h| h["images"] }
-    photos = photo_links.map { |photo_set| photo_set.values_at(3, 4) }
-  end
 end
