11b121510f49f9d78c92bffb28f5e8517e5ed591
Ralphos <wintle.ralph@gmail.com>
2012-08-17 18:28:24 +0800

    Cleaning up some code

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,10 @@ Friendskis::Application.routes.draw do
   match 'auth/failure', to: redirect('/')
   match 'signout', to: 'sessions#destroy', as: 'signout'
   
-  resources :photos, except: [:edit, :update]
+  resources :photos, except: [:new, :edit, :update]
   post 'photos/confirm', controller: 'photos', action: 'preview', as: :preview
   post '/album', controller: 'photos', action: 'album', as: :album_view
+  get 'albums', controller: 'photos', action: 'new', as: :albums
   
   get '/feed', controller: 'users', action: 'index', as: :users
   
