0b2f79b3004be074be1eb1ab9cc93610e7ab02f9
Ralphos <wintle.ralph@gmail.com>
2012-08-18 20:46:21 +0800

    Made to helper methods to display sex and preference on user profile page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,22 @@ module ApplicationHelper
   def profile_pic_medium
     image_tag(Photo.find(current_user.profile_pic).medium_url)
   end
+  
+  def display_sex(user)
+    if user.gender == "male"
+      "M" 
+    elsif user.gender == "female"
+      "F"
+    else
+      "-"
+    end 
+  end
+  
+  def criteria(user)
+    if user.preference == '1'
+      "Looking for men"
+    elsif user.preference == '2'
+      "Looking for women"
+    end
+  end
 end
