22912f9f9537815c6c3967c4f470601005f89228
Ralphos <wintle.ralph@gmail.com>
2012-08-29 15:36:41 +0800

    Got popup modal for messaging working on profile pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @photos = @user.latest_user_photos
+    @message = Message.new
   end
   
   def update
