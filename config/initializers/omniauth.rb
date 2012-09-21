5adc85fbb757fd45f26aded7cbfc60c89ec567e3
Ralphos <wintle.ralph@gmail.com>
2012-09-21 16:00:05 +0800

    Tried adding separate app id/key for production

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,11 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   # Set up ENV variables
-  provider :facebook, '386324544771493', '775dfc20bc0f9461e86a383d83b52a1e',
+  if Rails.env == "production"
+    provider :facebook, '386324544771493', '775dfc20bc0f9461e86a383d83b52a1e',
     scope: 'email,user_photos,user_birthday,user_location' 
+  else
+    provider :facebook, '360375230710287', 'b7781e53494b4ec7f7162b834fcbab57',
+    scope: 'email,user_photos,user_birthday,user_location' 
+  end
 end
