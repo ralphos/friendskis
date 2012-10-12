071bbad8ebd788080c055b64f60070e3248cdabb
Ralphos <wintle.ralph@gmail.com>
2012-10-12 12:04:16 +0800

    Begun work on adding facebook payment subscriptions

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,8 +5,10 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env == "production"
     provider :facebook, '386324544771493', '775dfc20bc0f9461e86a383d83b52a1e',
     scope: 'email,user_photos,user_birthday,user_location' 
+    FB_APP_ID = '386324544771493'
   else
-    provider :facebook, '360375230710287', 'b7781e53494b4ec7f7162b834fcbab57',
+    provider :facebook, '182721585186027', 'acce0a1b053db8895ab4f4892ca6c599',
     scope: 'email,user_photos,user_birthday,user_location' 
+    FB_APP_ID = '182721585186027'
   end
 end
