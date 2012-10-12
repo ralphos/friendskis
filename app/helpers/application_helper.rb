82eb0701605c165dfd813de6043e47200e4ef83f
Ralphos <wintle.ralph@gmail.com>
2012-10-12 12:02:31 +0800

    Added a helper for the popularity meter bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -49,5 +49,17 @@ module ApplicationHelper
       "Looking for women"
     end
   end
+
+  def popularity_label(user)
+    if current_user.percent_score <= 25
+      "<span class='label notice'>Low</span>".html_safe
+    elsif current_user.percent_score > 25 && current_user.percent_score <= 50
+      "<span class='label warning'>medium</span>".html_safe
+    elsif current_user.percent_score > 50 && current_user.percent_score <= 75 
+      "<span class='label success'>High</span>".html_safe
+    elsif current_user.percent_score > 75 && current_user.percent_score <= 100
+      "<span class='label important'>Hot</span>".html_safe
+    end
+  end
     
 end
