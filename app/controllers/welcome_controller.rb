0822111e5280446f8c18139172364606a77122f2
Ralphos <wintle.ralph@gmail.com>
2012-10-15 10:46:53 +0800

    Cleaned up some details relating to fb subscriptions

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,22 +7,6 @@ class WelcomeController < ApplicationController
   def index
   end 
 
-  def payments_callback
-    items = {
-      content: [
-              {
-                title: "BFF Locket",
-                description: "Best friend locket",
-                price: 1,
-                image_url: "http:\/\/www.facebook.com\/images\/gifts\/21.png"
-              }
-            ],
-      method: "payments_get_items"
-    }
-
-    render json: items, layout: false 
-  end
-
   def subscription
     render layout: false
   end
