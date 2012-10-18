ce69d4b3d38e7d16fe82242c925736a0fb94faac
Ralphos <wintle.ralph@gmail.com>
2012-10-18 11:30:39 +0800

    Added pagination using kaminari

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,13 @@
 class UsersController < ApplicationController
   
   def index
-    @photos = UserPhoto.latest_photos(current_user)
+    @photos = UserPhoto.latest_photos(current_user).page(params[:page])
     @message = Message.new
   end
   
   def show
     @user = User.find(params[:id])
-    @photos = @user.recent_photos
+    @user_photos = @user.recent_photos.page(params[:page])
     @message = Message.new
     Visitor.find_or_create_by_visitor_id_and_user_id(current_user.id, params[:id]) 
   end
