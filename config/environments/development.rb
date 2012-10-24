abeb5d3f2141d2e7824953c33bc13b89df1df475
Ralphos <wintle.ralph@gmail.com>
2012-10-24 17:19:50 +0800

    Added sendgrid config to development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,18 @@ Friendskis::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # EY Sendgrid config
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_charset = "utf-8"
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+       :authentication => :plain,
+       :address => "smtp.sendgrid.net",
+       :port => 587,
+       :domain => EY::Config.get(:sendgrid, 'smtp.sendgrid.net'),
+       :user_name => EY::Config.get(:sendgrid, 'ey_service_1874@engineyard.com'),
+       :password => EY::Config.get(:sendgrid, 'tyuyxnrs')
+  }
 end
