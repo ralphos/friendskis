00e7f04c12be140e5593c14f244f1563dc05b1b2
Ralphos <wintle.ralph@gmail.com>
2012-10-25 14:44:09 +0800

    Clean up on homepage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -15,13 +15,11 @@ class WelcomeController < ApplicationController
       return
     end
 
-#    if User.all.any?
       #users = User.recent.slice(-4, 4)
       #@user1 = users[0]
       #@user2 = users[1]
       #@user3 = users[2]
       #@user4 = users[3]
-    #end
   end 
 
   def subscription
