e533d02ad75934ef1fd4c6a18c8ee1e27bf9bd97
Ralphos <wintle.ralph@gmail.com>
2012-10-29 13:46:19 +0800

    Making homepage static

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -17,10 +17,10 @@ class WelcomeController < ApplicationController
 
     if User.all.count >= 5
       users = User.all.slice(-4, 4)
-      @user1 = users[0]
-      @user2 = users[1]
-      @user3 = users[2]
-      @user4 = users[3]
+      #@user1 = users[0]
+      #@user2 = users[1]
+      #@user3 = users[2]
+      #@user4 = users[3]
     end
   end 
 
