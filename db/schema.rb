b4daa172188a9161c13944b0ea4e81e10293420c
Jason Torres <jason.e.torres@gmail.com>
2012-10-29 16:57:20 +0800

    Start using activerecord session migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121018050413) do
+ActiveRecord::Schema.define(:version => 20121029085701) do
 
   create_table "conversations", :force => true do |t|
     t.integer  "sender_id"
@@ -51,6 +51,16 @@ ActiveRecord::Schema.define(:version => 20121018050413) do
     t.datetime "updated_at",      :null => false
     t.datetime "rank_updated_at"
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "user_photos", :force => true do |t|
     t.integer  "user_id"
