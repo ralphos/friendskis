3aca116bd607ff647c3ab75cb7190838b02ec6cc
Ralphos <wintle.ralph@gmail.com>
2012-10-29 13:53:56 +0800

    Made homepage static

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -14,14 +14,6 @@ class WelcomeController < ApplicationController
       redirect_to "https://apps.facebook.com/friendskis/"
       return
     end
-
-    if User.all.count >= 5
-      users = User.all.slice(-4, 4)
-      #@user1 = users[0]
-      #@user2 = users[1]
-      #@user3 = users[2]
-      #@user4 = users[3]
-    end
   end 
 
   def subscription
