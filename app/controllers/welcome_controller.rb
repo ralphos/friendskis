0d96a31142876303a216fa2a3abce11ae8bc6f1f
Ralphos <wintle.ralph@gmail.com>
2012-10-29 14:10:44 +0800

    swapped code in the index action

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,16 +5,16 @@ class WelcomeController < ApplicationController
   layout 'welcome' 
   
   def index
-    if user_signed_in?
-      redirect_to users_path
-      return
-    end
-
     if Rails.env == "production" && params[:signed_request].blank?
       redirect_to "https://apps.facebook.com/friendskis/"
       return
     end
-  end 
+
+    if user_signed_in?
+      redirect_to users_path
+      return
+    end 
+  end
 
   def subscription
     render layout: false
