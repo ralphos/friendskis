a843143a5e7254488f7c3206a90b935ce48b51e3
Jason Torres <jason.e.torres@gmail.com>
2012-11-07 15:35:06 +0800

    Attempt fix by using Koala and parsing the signed_request instead for non-session persistent browsers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,12 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if @current_user.blank? && session[:user_id]
-    @current_user ||= User.where(uid: @fb_session_id).first if @current_user.blank? && @fb_session_id.present?
+
+    # If the sign in from facebook didn't work. Get from signed request (Safari workaround)
+    if @current_user.blank? && fb_user_id.present?
+      @current_user ||= User.where(uid: fb_user_id).first if fb_user_id
+    end
+
     @current_user
   end
 
@@ -51,18 +56,14 @@ class ApplicationController < ActionController::Base
     Rails.logger.info "Signed Request: #{params[:signed_request]}"
   end
 
+  def fb_user_id
+    koala = Koala::Facebook::OAuth.new '386324544771493', '775dfc20bc0f9461e86a383d83b52a1e'
+    @fb_user_id = koala.parse_signed_request(params[:signed_request])
+  end
+
   def detect_facebook
-    Rails.logger.info "Request Headers: #{request.headers.keys}"
-
-    @fb_session_id = request.headers['HTTP_X_FB_ID']
-
-    logger.info "FACEBOOK ID: #{@fb_session_id} / FB PARAMS #{request.env['facebook.params']}"
-
-    if request.env['facebook.params']
-      #logger.info "Received POST w/ signed_request from Facebook."
-      #log_in_with_facebook request.env['facebook.params']
-    end
-    true
+    @fb_user_id = request.headers['HTTP_X_FB_ID']
+    logger.info "FACEBOOK ID: #{@fb_user_id}"
   end
 
 end
