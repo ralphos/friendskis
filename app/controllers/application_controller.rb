34bcf01ef1e3e04555b936dc73b676f60bbc942a
Jason Torres <jason.e.torres@gmail.com>
2012-11-07 16:14:08 +0800

    More attempt fix for fb_user_id detection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,10 +10,12 @@ class ApplicationController < ActionController::Base
   helper_method :correct_user?
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if @current_user.blank? && session[:user_id]
+    if session[:user_id].present?
+      @current_user ||= User.find(session[:user_id])
+    end 
 
     # If the sign in from facebook didn't work. Get from signed request (Safari workaround)
-    if @current_user.blank? && fb_user_id.present?
+    unless @current_user
       @current_user ||= User.where(uid: fb_user_id).first if fb_user_id
     end
 
@@ -56,9 +58,30 @@ class ApplicationController < ActionController::Base
   end
 
   def fb_user_id
-    koala = Koala::Facebook::OAuth.new '386324544771493', '775dfc20bc0f9461e86a383d83b52a1e'
-    signed_params = koala.parse_signed_request(params[:signed_request])
-    @fb_user_id = signed_params["user"]["user_id"] if signed_params && signed_params["user"]
+    if params[:signed_request]
+      koala = Koala::Facebook::OAuth.new FB_APP_ID, FB_SECRET
+
+      signed_params = koala.parse_signed_request(params[:signed_request])
+      @fb_user_id = signed_params["user_id"] if signed_params
+
+      session[:fb_user_id] = cookies[:fb_user_id] = @fb_user_id if @fb_user_id.present?
+    end
+
+    if @fb_user_id.blank?
+      @fb_user_id = request.headers['HTTP_X_FB_ID']
+      session[:fb_user_id] = cookies[:fb_user_id] = @fb_user_id if @fb_user_id.present?
+    end
+
+    if @fb_user_id.blank? && session[:fb_user_id]
+      @fb_user_id = session[:fb_user_id]
+      session[:fb_user_id] = cookies[:fb_user_id] = @fb_user_id if @fb_user_id.present?
+    end
+
+    if @fb_user_id.blank? && cookies[:fb_user_id]
+      @fb_user_id = cookies[:fb_user_id]
+      session[:fb_user_id] = cookies[:fb_user_id] = @fb_user_id if @fb_user_id.present?
+    end
+
   end
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,23 +9,24 @@ class SessionsController < ApplicationController
 
     if user.is_first_login? 
       user.get_birthday
-      redirect_to step_one_path
+      redirect_to step_one_path(signed_request: params[:signed_request])
     elsif user.profile_pic.nil?
-      redirect_to step_two_path
+      redirect_to step_two_path(signed_request: params[:signed_request])
     else
-      redirect_to users_path(id: user.id)
+      redirect_to users_path(signed_request: params[:signed_request])
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_path
+    redirect_to root_path(signed_request: params[:signed_request])
   end
 
   def test_login
     if Rails.env.test?
       session[:user_id] = params[:user_id]
     end
-    redirect_to users_path
+    redirect_to users_path(signed_request: params[:signed_request])
+
   end
 end
