f9fe6d9b9131a2483fbdf4c453790aaa401551bb
Ralphos <wintle.ralph@gmail.com>
2012-11-12 15:18:38 +0800

    sales dialog ready for action

--- app/assets/javascripts/sales_dialog.js
+++ app/assets/javascripts/sales_dialog.js
@@ -1,7 +1,7 @@
 function salesModal(e) {
   
   $('#sales-modal').modal({show:true, backdrop: true});        
-  $('#sales-modal').find('.modal-header').html("<h3>Become a subscriber</h3>");
+  $('#sales-modal').find('.modal-header').html("<h3>Become a subscriber & send unlimited messages!<h3>");
 
   $('#sales-click').bind('click', function() {
     $('#sales-modal').modal('hide');
