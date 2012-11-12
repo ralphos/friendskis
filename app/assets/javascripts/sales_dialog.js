b22cc53c28937b4f80ed4c3f18cf381361e2d965
Ralphos <wintle.ralph@gmail.com>
2012-11-12 14:33:02 +0800

    Modal now hides when you click pay on facebook

--- app/assets/javascripts/sales_dialog.js
+++ app/assets/javascripts/sales_dialog.js
@@ -3,5 +3,9 @@ function salesModal(e) {
   $('#sales-modal').modal({show:true, backdrop: true});        
   $('#sales-modal').find('.modal-header').html("<h3>Become a subscriber</h3>");
 
+  $('#sales-click').bind('click', function() {
+    $('#sales-modal').modal('hide');
+  });
+
 }
 
