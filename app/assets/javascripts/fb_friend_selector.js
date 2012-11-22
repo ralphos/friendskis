bbb4cf572d7a5a3408a46faeefb45d08738e4921
Ralphos <wintle.ralph@gmail.com>
2012-11-22 13:57:38 +0800

    Working on javascript for friend selector

--- app/assets/javascripts/fb_friend_selector.js
+++ app/assets/javascripts/fb_friend_selector.js
@@ -4,7 +4,18 @@ function sendRequestViaMultiFriendSelector() {
     }, requestCallback);
 }
 
-function requestCallback(response) { 
-  // Handle callback here
+function requestCallback(response) {
+
+  $.ajax({
+    url: "/invites",
+    type: 'POST',
+    data: { ids: response.to, fb_request_id: response.request },
+    success: function(e) {
+      if(e.success == true)  {
+      } else {
+        alert("Sorry! Something went wrong.") 
+      }
+    }
+ })
 
 }
