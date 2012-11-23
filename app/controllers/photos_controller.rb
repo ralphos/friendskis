95b13a2d5cdf48ebd619ac7d57781be54175e9ee
Ralphos <wintle.ralph@gmail.com>
2012-11-23 15:00:35 +0800

    Fixed cool stats when you cool a photo

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -42,7 +42,7 @@ class PhotosController < ApplicationController
 
     respond_to do |format| 
       format.js {
-        render js: "$('##{dom_id(@photo, 'like')}').parent().html('You thought this photo was cool')" 
+        render js: "$('##{dom_id(@photo, 'like')}').parent().html('#{view_context.pluralize(@photo.likes.count, 'person')} thought this photo was cool')" 
       }
       format.html { redirect_to users_url }
     end
